b42de516def843e85bca8082dda2abb4a2868565
Charlie Theodore <charliestableford2@gmail.com>
2020-09-05 12:39:30 -0700

    progress

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-console.log('made it');
-
-
 const searchUpdate = document.querySelector('.searchUpdate');
 const searchBtn = document.querySelector('.searchBtn');
 const nominationsUL = document.querySelector('.nominations');
 const movieList = document.querySelector('.movieInfo');
+const finished = document.querySelector('.banner');
 
-const nomArray = [];
-// const title = 'batman';
+let nomArray = [];
+let counter = 0;
 const key = `406fb5b1`;
-// const endpoint = `https://www.omdbapi.com/?apikey=${key}&s=${title}`;
 
 function searchInputHandler(e) {
     e.preventDefault();
@@ -23,9 +20,14 @@ async function ApiHandler(title) {
     if (searching.Response === "True") {
         // let search = [];
         // search.push(searching);
-        // console.log(search);
+        console.log(searching.Search);
         display(searching.Search);
-    } 
+    } else {
+        movieList.innerHTML = `
+        <div class="noTitle">
+        sorry we have nothing by that name!
+        </div>`;
+    }
 }
 
 // RENDER MOVIE RESULTS
@@ -38,21 +40,30 @@ function display(results) {
     ).join('');
 }
 
+// <li><img src="${movie.Poster}" alt="${movie.Title} ${movie.Type} poster"></li>
+
 // RENDER NOMINATIONS LIST
     async function renderNom(idMovie){ 
         console.log(idMovie);
-
+        // make another call based on the movie id stored
         const response = await fetch(`https://www.omdbapi.com/?apikey=${key}&i=${idMovie}`);
         const nomSearch = await response.json();
         if (nomSearch.Response === "True") {
-        console.log(nomSearch);     
+        console.log(nomSearch);   
+        // add the search result to the nomArray  
         nomArray.push(nomSearch);
-        console.log(nomArray);
+
+        //check each click, if there are more then 5 nominations then show banner
+        if(nomArray.length >= 5){
+            finished.classList.add("show");
+        }
+
         nominationsUL.innerHTML = nomArray.map(nomSearch =>
-            `<ul class="NomUL">
-                        <li>${nomSearch.Title} - ${nomSearch.Year} <button class="no" value="${nomSearch.imdbID}">Remove</button></li>
-                    </ul>`
-        );
+            `<div class="num"></div>
+            <ul class="NomUL">
+                <li>${nomSearch.Title} - ${nomSearch.Year} / ${nomSearch.imdbRating} <button class="remove" value="${nomSearch.imdbID}">Remove</button></li>
+            </ul>`
+        ).join('');
     }
  }
 
@@ -60,18 +71,31 @@ function nominate(e) {
     if (e.target.classList.contains('nominate')) {
         // getting the movie ID
         let idMovie = e.target.getAttribute('value');
-        //push values of movies into a new array
-
+        e.target.disabled = true;
         renderNom(idMovie);
-        // console.log(nomArray);
+
         // addToLocalstorage(e.target.getAttribute('value'));
         // checkLocalStorage();
     }
 }
 
+function removeNom(e){
+    if(e.target.classList.contains('remove')){
+        // console.log(e.target.getAttribute('value'));
+        let clicked = e.target.getAttribute('value');
+        delete nomArray.clicked;
+        // console.log('remove');
+        // console.log(nomArray);
+        console.log(nomArray);
+        // removeToLocalstorage(e.target.getAttribute('value'));
+    }
+}
+
+
+
 window.addEventListener('click', nominate);
+window.addEventListener('click', removeNom);
 searchUpdate.addEventListener('keyup', searchInputHandler);
 searchBtn.addEventListener('click', searchInputHandler);
 
-// ApiHandler();
 

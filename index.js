daa2557f0927a708f30538964ec1e3adb46b2907
Charlie Theodore <charliestableford2@gmail.com>
2020-09-05 10:05:42 -0700

    adding to the nominations list

--- index.js
+++ index.js
@@ -1,72 +1,77 @@
 console.log('made it');
 
 
-const searchInput = document.querySelector('.search');
-const title = 'batman';
+const searchUpdate = document.querySelector('.searchUpdate');
+const searchBtn = document.querySelector('.searchBtn');
+const nominationsUL = document.querySelector('.nominations');
+const movieList = document.querySelector('.movieInfo');
+
+const nomArray = [];
+// const title = 'batman';
 const key = `406fb5b1`;
-const endpoint = `https://www.omdbapi.com/?apikey=${key}&s=${title}`;
-const movieList = document.querySelector('.movieBox');
+// const endpoint = `https://www.omdbapi.com/?apikey=${key}&s=${title}`;
 
-
-async function ApiHandler(title) {
-    const response = await fetch(endpoint);
-    const searching = await response.json();
-    if (searching.Response === "True") {
-        console.log(searching);
-        display(searching.Search);
-
-    }
+function searchInputHandler(e) {
+    e.preventDefault();
+    const textSearch = searchUpdate.value;
+    ApiHandler(textSearch);
 }
 
-// function displayMatches(results) {
-//     const matchArray = findMatches(this.value, results);
-//     const html = matchArray.map(results => {
-//       const regex = new RegExp(this.value, 'gi');
-//       const movieTitle = results.movies.replace(regex, `<span class="hl">${this.value}</span>`);
-//       return `
-//         <li>
-//           <span class="name">${movieTitle}</span>
-//         </li>
-//       `;
-//     }).join('');
-//     suggestions.innerHTML = html;
-//   }
+async function ApiHandler(title) {
+    const response = await fetch(`https://www.omdbapi.com/?apikey=${key}&s=${title}`);
+    const searching = await response.json();
+    if (searching.Response === "True") {
+        // let search = [];
+        // search.push(searching);
+        // console.log(search);
+        display(searching.Search);
+    } 
+}
 
-//   function findMatches(wordToMatch, title) {
-//     return title.filter(results => {
-//       // here we need to figure out if the city or state matches what was searched
-//       const regex = new RegExp(wordToMatch, 'gi');
-//       return results.city.match(regex) || results.state.match(regex)
-//     });
-//   }
-  
-
+// RENDER MOVIE RESULTS
 function display(results) {
+
     movieList.innerHTML = results.map(movie =>
-        `<div class="movieInfo">
-                <div class="info">
-                    <p>${movie.Title} - ${movie.Year}</p>
-                </div>
-                <button class="nominate" value="${movie.imdbID}">Nominate</button>
-        </div>`
+        `<ul class="info">
+                    <li>${movie.Title} - ${movie.Year} <button class="nominate" value="${movie.imdbID}">Nominate</button></li>
+                </ul>`
     ).join('');
 }
 
+// RENDER NOMINATIONS LIST
+    async function renderNom(idMovie){ 
+        console.log(idMovie);
+
+        const response = await fetch(`https://www.omdbapi.com/?apikey=${key}&i=${idMovie}`);
+        const nomSearch = await response.json();
+        if (nomSearch.Response === "True") {
+        console.log(nomSearch);     
+        nomArray.push(nomSearch);
+        console.log(nomArray);
+        nominationsUL.innerHTML = nomArray.map(nomSearch =>
+            `<ul class="NomUL">
+                        <li>${nomSearch.Title} - ${nomSearch.Year} <button class="no" value="${nomSearch.imdbID}">Remove</button></li>
+                    </ul>`
+        );
+    }
+ }
+
 function nominate(e) {
     if (e.target.classList.contains('nominate')) {
-        // getting the movie ID.
-        value = e.target.getAttribute('value');
-        console.log('nominate');
-        console.log(value)
+        // getting the movie ID
+        let idMovie = e.target.getAttribute('value');
+        //push values of movies into a new array
+
+        renderNom(idMovie);
+        // console.log(nomArray);
         // addToLocalstorage(e.target.getAttribute('value'));
         // checkLocalStorage();
     }
 }
 
 window.addEventListener('click', nominate);
-// searchInput.addEventListener('change', displayMatches);
-// searchInput.addEventListener('keyup', displayMatches);
-// searchInput.addEventListener('submit', ApiHandler());
+searchUpdate.addEventListener('keyup', searchInputHandler);
+searchBtn.addEventListener('click', searchInputHandler);
 
 // ApiHandler();
 
